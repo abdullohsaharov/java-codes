22a702f3f4681da089224d6d8cb9f9fa32af846e
abdullohsaharov <abdullohsaharov72@gmail.com>
2025-10-25 16:42:04 +0500

    Add files via upload

---
+++ Arraylist/Main.java
@@ -0,0 +1,114 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Collections;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static jdk.jfr.internal.util.Utils.compareLists;
+
+public class Main {
+   public static void main(String[] args) {
+//     ArrayList<String> color=new ArrayList<>();
+//     color.add("red");
+//     color.add("black");
+//     color.add("yellow");
+//     color.add("blue");
+//     color.add("white");
+//     color.add("pink");
+//     for(String rang:color){
+//        System.out.println(rang);
+//     }
+//      color.set(0,"green");
+//      for (int i=0;i< color.size();i++){
+//      System.out.println(color.get(i));
+//      System.out.println(color.size());
+//     }
+//
+//   }
+//
+//       ArrayList<Integer> info = new ArrayList<>();
+//       Random random = new Random();
+//
+//       for (int i = 0; i < 10; i++) {
+//           int rand = random.nextInt(100);
+//           System.out.println("random " + rand);
+//           info.add(rand);
+//       }
+//
+//       System.out.println("Index of 100: " + info.indexOf(12));
+//       ArrayList<Integer> copy=new ArrayList<>(info);
+//       System.out.println(" "+copy);
+//       Collections.shuffle(copy);
+//       System.out.println("Mixed (shuffled) list: " + copy);
+//
+//       ArrayList<Double> numbers = new ArrayList<>();
+//       Random rand = new Random();
+//
+//       for (int i = 0; i < 10; i++) {
+//           double sample = rand.nextDouble();
+//           numbers.add(sample);
+//       }
+//
+//       System.out.println("Random elements:");
+//       for (Double num : numbers) {
+//           System.out.println(num);
+//       }
+//
+//       // Reverse the list
+//       Collections.reverse(numbers);
+//
+//       System.out.println("\nReversed order:");
+//       for (Double num : numbers) {
+//           System.out.println(num);
+//       }
+//
+//       // Create another ArrayList of integers
+//       ArrayList<Integer> number = new ArrayList<>();
+//       for (int i = 1; i <= 10; i++) {
+//           number.add(i);
+//       }
+//
+//       System.out.println("\nOriginal integer list: " + number);
+//
+//       int firstIndex = 3;
+//       int lastIndex = 7;
+//
+//       // Check for valid indices
+//       if (firstIndex < 0 || lastIndex >= number.size() || firstIndex > lastIndex) {
+//           System.out.println("Invalid index range!");
+//           return;
+//       }
+//
+//       // Get a sublist (note: subList's end index is exclusive, so +1)
+//       List<Integer> subList = new ArrayList<>(number.subList(firstIndex, lastIndex + 1));
+//
+//       System.out.println("Sublist (" + firstIndex + "–" + lastIndex + "): " + subList);
+//   }
+
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
